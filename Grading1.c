7f57af143425f298012bdf3cccaae94273e0b84c
nared456 <78350512+nared456@users.noreply.github.com>
2021-02-08 16:45:37 +0700

    Grade_6320503041

---
+++ Grading1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,grade = 0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    grade = a+b+c;
+    if(grade>=80 || grade<=100)
+    {
+        printf("A");
+    }
+    else if(grade>=75 || grade<=79)
+    {
+        printf("B+");
+    }
+    else if(grade>=70 || grade<=74)
+    {
+        printf("B");
+    }
+    else if(grade>=65 || grade<=69)
+    {
+        printf("C+");
+    }
+    else if(grade>=60 || grade<=64)
+    {
+        printf("C+");
+    }
+    else if(grade>=55 || grade<=59)
+    {
+        printf("D+");
+    }
+    else if(grade>=50 || grade<=54)
+    {
+        printf("D");
+    }
+    else
+    {
+        printf("F");
+    }
+    return 0;
+}
